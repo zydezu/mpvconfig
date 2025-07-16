d433956431dd7a26db8205063031c60d5b6ac896
zydezu <kinomaxxing@gmail.com>
2025-07-16 06:01:49 +0100

    FIX: update bat file

--- updatefiles.bat
+++ updatefiles.bat
@@ -1,19 +1,30 @@
 @echo off
 setlocal
 
+REM === Restrict script location ===
+set "EXPECTED_DIR=%APPDATA%\mpv"
+set "SCRIPT_DIR=%~dp0"
+REM Remove trailing backslash from SCRIPT_DIR if present
+if "%SCRIPT_DIR:~-1%"=="\" set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+if /i not "%SCRIPT_DIR%"=="%EXPECTED_DIR%" (
+    echo [ERROR] This script can only be run from: %EXPECTED_DIR%
+    pause
+    exit /b 1
+)
+
 REM === Configuration ===
 set "REPO_NAME=mpvconfig"
 set "BRANCH=main"
-set "SCRIPT_DIR=%~dp0"
-set "ZIP_PATH=%SCRIPT_DIR%%REPO_NAME%.zip"
-set "EXTRACT_DIR=%SCRIPT_DIR%repo_extracted"
+set "ZIP_PATH=%SCRIPT_DIR%\%REPO_NAME%.zip"
+set "EXTRACT_DIR=%SCRIPT_DIR%\repo_extracted"
 set "DOWNLOAD_URL=https://github.com/zydezu/%REPO_NAME%/archive/refs/heads/%BRANCH%.zip"
 
 REM Delete all files and folders except this script
-for /d %%D in ("%SCRIPT_DIR%*") do (
+for /d %%D in ("%SCRIPT_DIR%\*") do (
     if /i not "%%~nxD"=="%~nx0" rd /s /q "%%D"
 )
-for %%F in ("%SCRIPT_DIR%*") do (
+for %%F in ("%SCRIPT_DIR%\*") do (
     if /i not "%%~nxF"=="%~nx0" del /f /q "%%F"
 )
 
@@ -40,7 +51,7 @@ set "SUBFOLDER=%EXTRACT_DIR%\%REPO_NAME%-%BRANCH%"
 
 if exist "!SUBFOLDER!\" (
     echo [INFO] Updating files...
-    xcopy "!SUBFOLDER!\*" "%SCRIPT_DIR%" /s /e /y /h /i >nul
+    xcopy "!SUBFOLDER!\*" "%SCRIPT_DIR%\" /s /e /y /h /i >nul
 ) else (
     echo [ERROR] Could not find extracted subfolder: !SUBFOLDER!
     exit /b 1
